15c331209a61b01c4c1f7789015ee1fd8d5d2ce1
jongju <jongju4@gamil.com>
2017-03-02 15:57:09 +0900

    2

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+
+app.set("view engine", 'ejs');
+
+// ejs 사용안할경우 아래와같이 폴더 설정시 index.html render
+// app.use(express.static(path.join(__dirname + '/public')));
+
+
 app.get('/', function (req, res){
-    res.send('Hello World!');
+    res.render('main');
+    console.log("/ ejs");
 });
 
 app.listen(4000, function(){
